f02aba913f066d35befd887b93c0d7d78904d9fb
t4runsingh <tarun.singh@impakter.com>
2024-11-19 19:26:04 +0000

    test123

---
+++ src/app/components/store/dashboarsStatusStore.js
@@ -0,0 +1,9 @@
+import { create } from 'zustand'
+
+const dashboardrStatusStore = create((set) => ({
+    dashboardStatus: "Free", 
+    setDashboardStatus: (value) => set({ dashboardStatus: value }),
+
+}))
+
+export default dashboardrStatusStore;
